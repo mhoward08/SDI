02541340cf704f9f36ababf9437d872fce5715ca
mhoward08 <melanieh2008@fullsail.edu>
2015-05-07 12:16:19 -0400

    Day Two commits

---
+++ Daytwo/js/script.js
@@ -0,0 +1,24 @@
+/*
+ Melanie Howard
+ SDI 1505
+ Day Two
+ Fun with Data Types
+ */
+
+// This is a single-line comment
+
+// These are my variables
+var myHomeAddress   = "123 Elm Street";
+var milesToStore    = 8.5;
+var construction;
+var myFavoriteStore = "Publix Supermarket";
+
+// adding my confirm
+// confirm();
+construction = confirm("Is there construction? Click OK for yes, Cancel for no.");
+
+// my outputs
+console.log("I Live at " + myHomeAddress + "."); // this is my first output
+console.log("I shop at " + myFavoriteStore + ".");
+console.log("It's only " + milesToStore + " miles to the store.");
+console.log("It's " + construction + " that there is construction enroute.");
