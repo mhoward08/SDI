11cbcf3e19779782afc2945b26167b7a6c4e335f
mhoward08 <melanieh2008@fullsail.edu>
2015-05-16 15:52:19 -0400

    FinalCommit

--- Howard_Melanie_lab6arrays/Howard_Melanie_lab6.js
+++ Howard_Melanie_lab6arrays/Howard_Melanie_lab6.js
@@ -3,14 +3,14 @@
 
 
 //// functions
-//function basketballTeams() {
+function basketballTeams() {
+//
 ////
-//////
-////
-////    // local variables
-//    var sportsTeams = ["Pistons", "Cavaliers", "Heat", "Warriors"];
-//    var cities = ["Detroit ", "Cleveland ", "Miami ", "San Francisco "];
-////
+//
+//    // local variables
+    var sportsTeams = ["Pistons", "Cavaliers", "Heat", "Warriors"];
+    var cities = ["Detroit ", "Cleveland ", "Miami ", "San Francisco "];
+//
     // loops
     for (var i = 0; i < sportsTeams.length; i++) {
         console.log(cities[i] + sportsTeams[i] + " is my favorite basketball team.");
