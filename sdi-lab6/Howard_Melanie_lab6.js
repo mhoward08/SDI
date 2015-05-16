8fa159070daf492b48ccece175ba2af9258178fc
mhoward08 <melanieh2008@fullsail.edu>
2015-05-16 15:23:38 -0400

    FinalCommitLoopsandArrays

--- sdi-lab6/Howard_Melanie_lab6.js
+++ sdi-lab6/Howard_Melanie_lab6.js
@@ -11,20 +11,20 @@
 //    var sportsTeams = ["Pistons", "Cavaliers", "Heat", "Warriors"];
 //    var cities = ["Detroit ", "Cleveland ", "Miami ", "San Francisco "];
 ////
-////    // loops
-////    for (var i = 0; i < sportsTeams.length; i++) {
-////        console.log(cities[i] + sportsTeams[i] + " is my favorite basketball team.");
-////
-////    }
-////
-////    //array Methods
-////    sportsTeams.push("Spurs");
-////    cities.push("San Antonio ");
-////
-////    for (var i = 0; i < sportsTeams.length; i++) {
-////        console.log(cities[i] + sportsTeams[i] + " is my favorite basketball team.");
-////
-////
-////    }
-////}
-////basketballTeams();
+    // loops
+    for (var i = 0; i < sportsTeams.length; i++) {
+        console.log(cities[i] + sportsTeams[i] + " is my favorite basketball team.");
+
+    }
+
+    //array Methods
+    sportsTeams.push("Spurs");
+    cities.push("San Antonio ");
+
+    for (var i = 0; i < sportsTeams.length; i++) {
+        console.log(cities[i] + sportsTeams[i] + " is my favorite basketball team.");
+
+
+    }
+}
+basketballTeams();
