f2975a158eff531b716240584a9088bff2c02b09
mhoward08 <melanieh2008@fullsail.edu>
2015-05-19 09:19:24 -0400

    first commit for lab 6
    initial comments to describe problem

---
+++ DaySeven/js/script.js
@@ -0,0 +1,29 @@
+/**
+ * Created by melaniehoward on 5/19/15.
+ */
+
+// Melanie Howard
+// Day Seven
+// SDI 1505
+// More arrays, loops, and functions
+
+//alert("JavaScript works!");
+
+// Lab six review
+
+// function
+function authorFunction() {
+
+    // local arrays
+
+    // for loop to go through the array items
+    // output each element
+
+    // use an array method to alter the arrays
+
+    // do final output of the change
+
+
+}
+
+// main code
