b06a6c4350fe930e4d91cd65944c72567e2aff60
mhoward08 <melanieh2008@fullsail.edu>
2015-05-19 13:52:00 -0400

    FirstCommit

---
+++ sdi-lab7/rename-me.js
@@ -0,0 +1,14 @@
+//alert("JavaScript works!");
+
+//Melanie Howard
+//Day Seven
+//SDI 1505
+//Functions, Loops, and Arrays
+
+// global variables
+
+var numberArray1 = [10, 6, 7, 3, 12, 5];
+var numberArray2 = [25, 100, 4, 22, 16, 105, 2, 7];
+var numberArray3 = [2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42];
+var returnedArray = [];
+
