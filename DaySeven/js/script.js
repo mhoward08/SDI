70a47b9f9cfbe05b2d582f3481e23190dac4e353
mhoward08 <melanieh2008@fullsail.edu>
2015-05-19 12:49:40 -0400

    finalPractice

--- DaySeven/js/script.js
+++ DaySeven/js/script.js
@@ -10,21 +10,63 @@
 // global variables
 var numberArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ];
 var returnedArray = [];
+var userPrompt = prompt("Enter 'odd' to return odd numbers or 'even' to return even numbers.");
+
+
+while (userPrompt != "odd" && userPrompt != "even") {
+
+    userPrompt = prompt("Hey, stupid! Enter 'odd or 'even'");
+
+
+}
 
 // function
-function evenArray(argArray) {
+function evenArray(argArray, oddOrEven) {
 
     // local variables
     var newArray = [];
     var modNum;
+    var zeroOrOne;
 
-    console.log(argArray);
+    if (oddOrEven === "odd") {
+
+        zeroOrOne = 1;
+
+    } else {
+
+        zeroOrOne = 0
+
+    }
+
+
+    for (var i = 0; i < argArray.length; i++) {
+
+        modNum = argArray[i] % 2;
+
+        if (modNum === numberArray) {
+
+            newArray.push(numberArray[i]);
+
+
+        }
+
+
+    }
+
+    argArray.pop();
+    //console.log("argArray: " +  argArray);
+    //console.log("newArgArray: " + newArgArray);
+    return newArray;
 
 
 }
 
 // main code
-returnedArray = evenArray(numberArray);
+returnedArray = evenArray(numberArray, userPrompt);
+
+console.log("The " + userPrompt + " numbers array is " + returnedArray);
+
+console.log("numberArray: " + numberArray);
 
 
 
