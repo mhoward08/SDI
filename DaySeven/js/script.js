1b1eeb3a898a04371e70c4e5661b96f6198dac13
mhoward08 <melanieh2008@fullsail.edu>
2015-05-19 10:17:48 -0400

    First commit for evenArray

--- DaySeven/js/script.js
+++ DaySeven/js/script.js
@@ -7,29 +7,89 @@
 // SDI 1505
 // More arrays, loops, and functions
 
-//alert("JavaScript works!");
-
-// Lab six review
+// global variables
+var numberArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ];
+var returnedArray = [];
 
 // function
-function authorFunction() {
+function evenArray(argArray) {
 
-    // local arrays
-    var authors = [ "Charles Dickens", "William Shakespeare", "Mark Twain", "Hunter S. Thompson"];
-    var titles  = [ "Oliver Twist", "Hamlet", "Tom Sawyer", "Fear and Loathing in Las Vegas"];
+    // local variables
+    var newArray = [];
+    var modNum;
 
-    // for loop to go through the array items
-    // output each element
-    for (var i = 0; i < authors.length; i++) {
-
-        console.log(authors[i] + " wrote " + titles[i] + ".");
-
-    }
-    // use an array method to alter the arrays
-
-    // do final output of the change
+    console.log(argArray);
 
 
 }
 
 // main code
+returnedArray = evenArray(numberArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alert("JavaScript works!");
+
+// Lab six review
+
+// function
+//function authorFunction() {
+//
+//    // local arrays
+//    var authors = [ "Charles Dickens", "William Shakespeare", "Mark Twain", "Hunter S. Thompson"];
+//    var titles  = [ "Oliver Twist", "Hamlet", "Tom Sawyer", "Fear and Loathing in Las Vegas"];
+//
+//    // for loop to go through the array items
+//    // output each element
+//    for (var i = 0; i < authors.length; i++) {
+//
+//        console.log(authors[i] + " wrote " + titles[i] + ".");
+//
+//    }
+//    // use an array method to alter the arrays
+//    authors.push("Dorothy Allison");
+//    titles.push("Bastard Out of Carolina");
+//
+//    // do final output of the change
+//    console.log("I added " + authors[i] + " who wrote " + titles[i] + "." );
+//
+//
+//}
+//
+//// main code
+//
+//authorFunction();
