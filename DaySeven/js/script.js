4e83fc8aa095fd2287667e4e984b93b195d327eb
mhoward08 <melanieh2008@fullsail.edu>
2015-05-19 09:26:08 -0400

    Added a for loop

--- DaySeven/js/script.js
+++ DaySeven/js/script.js
@@ -15,10 +15,16 @@
 function authorFunction() {
 
     // local arrays
+    var authors = [ "Charles Dickens", "William Shakespeare", "Mark Twain", "Hunter S. Thompson"];
+    var titles  = [ "Oliver Twist", "Hamlet", "Tom Sawyer", "Fear and Loathing in Las Vegas"];
 
     // for loop to go through the array items
     // output each element
+    for (var i = 0; i < authors.length; i++) {
 
+        console.log(authors[i] + " wrote " + titles[i] + ".");
+
+    }
     // use an array method to alter the arrays
 
     // do final output of the change
