97d7f58a02e7b7025249b142bb9431e2432051f8
mhoward08 <melanieh2008@fullsail.edu>
2015-05-19 14:11:38 -0400

    SecondCommit
    Functions and Local variables

--- sdi-lab7/rename-me.js
+++ sdi-lab7/rename-me.js
@@ -12,3 +12,24 @@ var numberArray2 = [25, 100, 4, 22, 16, 105, 2, 7];
 var numberArray3 = [2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42];
 var returnedArray = [];
 
+
+// function
+
+function averageArray(argArray, numOfArrays) {
+
+    // local variables
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+// main code
+
