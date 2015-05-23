da1e6dd77a73d76967fe1d501b02b376a29db754
mhoward08 <melanieh2008@fullsail.edu>
2015-05-23 13:11:26 -0400

    problem1Commit

--- sdi-lab9/rename-me.js
+++ sdi-lab9/rename-me.js
@@ -1,1 +1,40 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+/*
+Melanie Howard
+SDI 1505
+Lab 9 - Number and Date Objects
+ */
+
+// global variables
+
+var num = 2.44494837587389475983;
+var amount = parseInt(prompt("What is the number? "));
+var returnedNum;
+
+
+
+// functions declarations
+// format a number
+
+function numberMethods(argPrompt, argPrompt2) {
+
+    var num1 = argPrompt.toFixed(argPrompt2);
+
+    return num1;
+
+
+
+
+
+
+}
+
+
+
+// main code
+
+var returnedNum = numberMethods(num, amount);
+console.log(returnedNum);
+
+
