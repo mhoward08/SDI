cfefeee1cb2737bf9028a85f55ec643f18be046b
mhoward08 <melanieh2008@fullsail.edu>
2015-05-23 13:41:14 -0400

    problem2Commit

--- sdi-lab9/rename-me.js
+++ sdi-lab9/rename-me.js
@@ -9,8 +9,10 @@ Lab 9 - Number and Date Objects
 // global variables
 
 var num = 2.44494837587389475983;
-var amount = parseInt(prompt("What is the number? "));
 var returnedNum;
+var newNum = (prompt("Enter a number please:"));
+var returnedVariable;
+
 
 
 
@@ -30,11 +32,36 @@ function numberMethods(argPrompt, argPrompt2) {
 
 }
 
+function numString(argPrompt) {
+
+    var results;
+
+
+    if (isNaN(argPrompt)) {
+         results = ("This is not a number.")
+
+    } else {
+      results = parseInt(argPrompt);
+
+
+    }
+
+    return results;
+}
+
 
 
 // main code
 
-var returnedNum = numberMethods(num, amount);
-console.log(returnedNum);
+////var returnedNum = numberMethods(num, amount);
+//var amount = parseInt(prompt("What is the number? "));
+////console.log(returnedNum);
+
+// main code problem 2
+
+var returnedVariable = numString(newNum);
+console.log(returnedVariable);
+
+
 
 
