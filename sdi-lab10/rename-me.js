a6be271c8c65770c063b2580d635abd7e57357a8
mhoward08 <melanieh2008@fullsail.edu>
2015-05-26 13:58:31 -0400

    FirstCommit
    global variable, function

--- sdi-lab10/rename-me.js
+++ sdi-lab10/rename-me.js
@@ -7,4 +7,18 @@ Lab Ten: Problem - Solving
 5/26/15
  */
 
+// global variable
+returnedLottery;
+
+// function
+function lotteryNumbers(lotteryType) {
+
+    // local variables
+    var powerBall = 
+
+}
+
+
+
+// main code
 
